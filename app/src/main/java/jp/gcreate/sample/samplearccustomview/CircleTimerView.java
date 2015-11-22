36e691b2d97d35de49c4341164914aa99017ae71
Gen <archiherewego@gmail.com>
2015-11-22 14:34:55 +0900

    Setting can modify with xml

--- app/src/main/java/jp/gcreate/sample/samplearccustomview/CircleTimerView.java
+++ app/src/main/java/jp/gcreate/sample/samplearccustomview/CircleTimerView.java
@@ -1,6 +1,7 @@
 package jp.gcreate.sample.samplearccustomview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,26 +14,43 @@ import android.view.View;
  */
 public class CircleTimerView extends View{
 
+    private static final float DEFAULT_MARGIN = 20f;
+    private static final int DEFAULT_BASE_COLOR = Color.rgb(200, 200, 200);
+    private static final int DEFAULT_BORDER_COLOR = Color.rgb(40, 8, 0);
     private RectF mRectF = new RectF();
     private Paint mPaint = new Paint();
     private Paint mPaintBase = new Paint();
     private float mArc = 300f;
+    private float mMargin;
 
     public CircleTimerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mPaint.setStrokeWidth(20f);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleTimerView);
+        mMargin = a.getDimension(R.styleable.CircleTimerView_margin,
+                getResources().getDisplayMetrics().density * DEFAULT_MARGIN);
+        float baseWidth = a.getDimension(R.styleable.CircleTimerView_base_width, 15f);
+        float borderWidth = a.getDimension(R.styleable.CircleTimerView_border_width, 20f);
+        int baseColor = a.getColor(R.styleable.CircleTimerView_base_color, DEFAULT_BASE_COLOR);
+        int borderColor = a.getColor(R.styleable.CircleTimerView_border_color, DEFAULT_BORDER_COLOR);
+        a.recycle();
+
+        mPaint.setStrokeWidth(borderWidth);
         mPaint.setAntiAlias(true);
-        mPaint.setColor(Color.rgb(40, 8, 0));
+        mPaint.setColor(borderColor);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaintBase.setStrokeWidth(20f);
+        mPaintBase.setStrokeWidth(baseWidth);
         mPaintBase.setAntiAlias(true);
-        mPaintBase.setColor(Color.rgb(200,200,200));
+        mPaintBase.setColor(baseColor);
+        mPaintBase.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        mRectF.set(getLeft(),getTop(),getWidth(),getHeight());
-        canvas.drawArc(mRectF,270f,360f,true,mPaintBase);
+        mRectF.set(getLeft(),
+                getTop(),
+                getRight(),
+                getBottom());
+        canvas.drawArc(mRectF, 270f, 360f, false, mPaintBase);
         canvas.drawArc(mRectF,270f,mArc,false,mPaint);
     }
 
@@ -44,5 +62,11 @@ public class CircleTimerView extends View{
     public void onFinishedToRestart(){
         mArc = 360f;
         invalidate();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int spec = Math.min(widthMeasureSpec, heightMeasureSpec);
+        super.onMeasure(spec, spec);
     }
 }
