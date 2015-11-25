d6583bad7b7764921513dfe22ab83d3d0bd8f943
Gen <archiherewego@gmail.com>
2015-11-25 16:12:06 +0900

    be simple

--- sample/src/main/java/jp/gcreate/sample/samplearccustomview/MainActivity.java
+++ sample/src/main/java/jp/gcreate/sample/samplearccustomview/MainActivity.java
@@ -3,8 +3,6 @@ package jp.gcreate.sample.samplearccustomview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import jp.gcreate.library.widget.circlebarbutton.CircleBarButton;
 
@@ -19,27 +17,5 @@ public class MainActivity extends AppCompatActivity {
 
         CircleBarButton target = (CircleBarButton) findViewById(R.id.circle);
         findViewById(R.id.button).setOnClickListener(view -> target.onFinishedToRestart());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
