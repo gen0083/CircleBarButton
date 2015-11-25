7ca630e53f83fc639f6be0a17ca47b59997b573f
Gen <archiherewego@gmail.com>
2015-11-25 16:45:08 +0900

    Update sample

--- sample/src/main/java/jp/gcreate/sample/samplearccustomview/MainActivity.java
+++ sample/src/main/java/jp/gcreate/sample/samplearccustomview/MainActivity.java
@@ -3,8 +3,13 @@ package jp.gcreate.sample.samplearccustomview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import java.util.concurrent.*;
 
 import jp.gcreate.library.widget.circlebarbutton.CircleBarButton;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +20,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        CircleBarButton target = (CircleBarButton) findViewById(R.id.circle);
+        final CircleBarButton target = (CircleBarButton) findViewById(R.id.circle);
+        final int count = 100;
+        target.setOnClickListener(view -> {
+            Observable.interval(10, TimeUnit.MILLISECONDS)
+                    .take(count + 1)
+                    .map(times -> count - times)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(time -> {
+                        float angle = ((float)time / (float)count) * 360f;
+                        Log.d("sample", "observable : time=" + time + ",angle=" + angle);
+                        target.rewriteCircle(angle);
+                    });
+        });
         findViewById(R.id.button).setOnClickListener(view -> target.onFinishedToRestart());
     }
 }
